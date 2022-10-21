fd9d97c3d1962a66b8c52eeb0b087d8db3d26bc3
Sebastian Näslund <sebastian.karl.naslund@gmail.com>
2022-10-21 16:33:10 +0200

    Image

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -3,11 +3,16 @@ interface CardProps {
     text: string
     image?: string
 }
-export const Card = ({ title, text }: CardProps) => {
+export const Card = ({ title, text, image }: CardProps) => {
     return (
-        <div className="bg-basseColor">
-            <h3>{title}</h3>
-            <p>{text}</p>
+        <div className="bg-basseColor relative">
+            <div className="z-2">
+                <h3>{title}</h3>
+                <p>{text}</p>
+            </div>
+            {image && (
+                <img src={image} className="absolute inset-0 opacity-50" />
+            )}
         </div>
     )
 }
