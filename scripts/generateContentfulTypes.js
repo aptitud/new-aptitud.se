e458dbe18b7585fbcacca103cf42b6bcf9a9f17c
Sebastian Näslund <sebastian.karl.naslund@gmail.com>
2022-10-21 19:57:11 +0200

    WIP generating types

---
+++ scripts/generateContentfulTypes.js
@@ -0,0 +1,7 @@
+const contentTypeGenerator = require('cf-content-types-generator')
+
+const { CONTENTFUL_MANAGEMENT_API_TOKEN } = process.env
+//()
+const client = new contentTypeGenerator()
+client.argv = ['-s', 'kqhdnxbobtly', '-t', CONTENTFUL_MANAGEMENT_API_TOKEN]
+client.run()
