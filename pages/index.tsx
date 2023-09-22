8e943cc931a1a0acdd32d89942388c453b3150dc
Joakim Wagstrom <Jocke@joakims-mbp-3.lan>
2023-09-22 11:24:35 +0200

    logo fix: size and transparent

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage<HomeProps> = ({ items }) => {
       <main>
         <div className="grid grid-cols-2 gap-4 xl:grid-cols-4">
           <div className="col-span-2 xl:col-span-4 ml-auto mr-auto my-2">
-            <Image src={'/logo.png'} height="210px" width="300px" />
+            <Image src={'/logo.svg'} height="302px" width="500px" />
           </div>
           {items.map((item) => {
             item
