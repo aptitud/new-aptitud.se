87f1ec372aac89c35b79d779ee32a3d9af7740db
jelmstrom <johan.elmstrom@gmail.com>
2023-10-04 20:46:37 +0200

    image as background

--- components/card/CardImage.tsx
+++ components/card/CardImage.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { CardVideo } from './CardVideo'
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, CSSProperties } from 'react';
 
 export const CardImage = ({
   image,
@@ -28,19 +28,24 @@ export const CardImage = ({
     };
   }, []);
 
+  const imageBg : CSSProperties =  {
+      backgroundImage: `url('${image ? image : '/logo.svg'}')`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }
   return (
     /**
      * TODO: Få video och stillbild att bli exakt lika stora, responsiva och 
      * på samma plats så att det inte blir något "hopp" när videon byts till stillbild.
      */
 
-    <div className='h-full w-full'>
+    <div className='h-full w-full rounded-lg'
+      style={imageBg}
+    >
       {
         video && showVideo ? 
         <CardVideo url={video} />
-        : image? 
-          <Image src={`https:${image}`} layout="fill" objectFit="cover" alt="asdf" className={"rounded-lg"} />
-          : <Image src="/logo.svg" alt="asdf" layout="fill" className={"rounded-lg"} />
+        : <></>
       }
     </div>
   )
