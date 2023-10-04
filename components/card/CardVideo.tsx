a52f1091ea0b30fd97174d32c3c6a3a641c61db4
PetterZiden <pziden@gmail.com>
2023-10-04 15:32:56 +0200

    Fixade videons storlek så att det matchar bilden

--- components/card/CardVideo.tsx
+++ components/card/CardVideo.tsx
@@ -1,11 +1,13 @@
+import {CSSProperties} from "react";
+
 export const CardVideo = ({
   url,
 }: {
   url: string
 }) => {
   return  (
-  <video autoPlay src= { url } muted loop>
-    Your browser does not support the video tag.
-  </video>
+        <video autoPlay src= { url } muted loop className="fellow-video">
+          Your browser does not support the video tag.
+        </video>
   )
 }
