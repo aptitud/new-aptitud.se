188d9e08ff6b26b5c0cec95a422d8d669175ebc8
jelmstrom <johan.elmstrom@gmail.com>
2023-10-06 16:59:58 +0200

    absolute element & display block / none istället för att rendera ut komponenten efter delay så videon förladdas

--- components/card/CardImage.tsx
+++ components/card/CardImage.tsx
@@ -1,4 +1,5 @@
 import { CardVideo } from './CardVideo'
+import Image from 'next/image'
 import React, { useState, useEffect, useRef, CSSProperties } from 'react';
 
 export const CardImage = ({
@@ -35,16 +36,18 @@ export const CardImage = ({
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   }
+  const videoBg: CSSProperties = {
+    display: showVideo ? 'block' : 'none',
+  }
+  
   return (
 
-    <div className='h-full w-full rounded-lg'
-      style={imageBg}
-    >
-      {
-        video && showVideo ?
-          <CardVideo url={video} />
-          : <></>
-      }
+    <div className='h-full w-full '>
+      <div className="absolute rounded-lg h-full w-full overflow-hidden" style={imageBg}>
+      </div>
+      <div className='absolute rounded-lg h-full w-full' style={videoBg} >
+        <CardVideo url={video || ''}  />
+      </div>
     </div>
   )
 }
