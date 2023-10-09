df539284c840e65a0ba6be2311a0f918900be0c2
jelmstrom <johan.elmstrom@gmail.com>
2023-10-09 09:44:40 +0200

    regular image tag

--- components/card/CardImage.tsx
+++ components/card/CardImage.tsx
@@ -1,6 +1,3 @@
-import { CardVideo } from './CardVideo'
-import Image from 'next/image'
-import React, { CSSProperties } from 'react';
 
 export const CardImage = ({
   image
@@ -11,17 +8,12 @@ export const CardImage = ({
 }) => {
 
 
-  
-  const imageBg: CSSProperties = {
-    backgroundImage: `url('https://${image}')`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  }
 
   return (
 
-    <div className='h-full w-full rounded-lg' style={imageBg}>
+    <div className='h-full w-full rounded-lg' >
       <div className="absolute h-full w-full rounded-lg" >
+          <img src={image ?'https://'+image : '/logo.svg'} className='object-fill' />
       </div>
     </div>
   )
