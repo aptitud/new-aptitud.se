e8819e419041ca83b055ce29d06d9671f17fb5b1
jelmstrom <johan.elmstrom@gmail.com>
2023-10-13 16:24:05 +0200

    fixa marginaler

--- components/card/FellowCard.tsx
+++ components/card/FellowCard.tsx
@@ -29,7 +29,7 @@ export const FellowCard = ({
     const options = {
       root: null, // Use the viewport as the root
       rootMargin: '0px',
-      threshold: 0.5, // Trigger when at least 50% of the element is in the viewport
+      threshold: 0.2, // Trigger when at least 50% of the element is in the viewport
     };
     
     const target = document.getElementById(image || '');
@@ -80,7 +80,7 @@ export const FellowCard = ({
         <div className="h-3/5">
 
         </div>
-        <div className={`h-2/5 text-white px-3 pb-3 pt-10 md:px-4 md:pb-6 md:pt-8`}>
+        <div className={`h-2/5 text-white px-3 pb-4 md:px-4 md:pb-6`}>
           <div className="grid grid-cols-1 relative h-full">
             <h3 className="text-base md:text-2xl mb-1 md:mb-1 font-medium truncate">{title}</h3>
             <span className='text-xs md:text-lg line-clamp-3  md:line-clamp-3'>
