bec00a04e1a8f52181439b9809975dfecb2ed3d5
Isa Jansson <isa.jansson@aptitud.se>
2023-11-24 10:54:58 +0100

    updated readme and changed icon for menu

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 copy `template.env` and name it `.env.local`
 set the value for `CONTENTFUL_ACCESS_TOKEN`
+and `INSTAGRAM_ACCESS_TOKEN`, fetch it from vercel
 
-Copy `CONTENTFUL_ACCESS_TOKEN` from  https://vercel.com/aptitud/new-aptitud-se/settings/environment-variables
+Copy `CONTENTFUL_ACCESS_TOKEN` from https://vercel.com/aptitud/new-aptitud-se/settings/environment-variables
 Generate a `CONTENTFUL_MANAGEMENT_API_TOKEN` from : https://app.contentful.com/spaces/kqhdnxbobtly/api/cma_tokens
 
 Then run the development server:
--- components/FilterMenu.tsx
+++ components/FilterMenu.tsx
@@ -2,8 +2,7 @@ import { FilterMenuProps } from './card/types'
 import { Contact } from './card/Contact'
 import { useRouter } from 'next/router'
 import {
-  DoubleArrowRightIcon,
-  DoubleArrowLeftIcon,
+  HamburgerMenuIcon,
   StarIcon,
   InstagramLogoIcon,
   PersonIcon,
@@ -35,11 +34,10 @@ export const FilterMenu = ({ contact }: FilterMenuProps) => {
           {filter !== '' && (
             <div className="bg-aptitud-petrol rounded-full h-3 w-3 absolute -right-1 -top-1 group-hover:invisible"></div>
           )}
-          <div className="peer cursor-pointer  rounded-tl-sm rounded-bl-sm md:rounded-tr-sm md:rounded-br-sm bg-white text-black p-3 bg-opacity-80 hover:bg-opacity-0">
-            <DoubleArrowRightIcon className="hidden md:block lg:block" />
-            <DoubleArrowLeftIcon className="block md:hidden lg:hidden" />
+          <div className="peer cursor-pointer rounded-tl-sm rounded-bl-sm md:rounded-tr-sm md:rounded-br-sm bg-white text-black p-3 bg-opacity-80 hover:bg-opacity-0">
+            <HamburgerMenuIcon />
           </div>
-          <ul className="w-0 shadow-lg invisible rounded-tl-sm rounded-bl-sm md:rounded-tr-sm md:rounded-br-sm transition-all duration-500 peer-hover:w-36 peer-hover:visible hover:w-36 hover:visible absolute top-0 right-0 md:right-auto md:left-0 bg-white text-black p-3 text-md">
+          <ul className="w-0 shadow-lg invisible h-44 overflow-hidden rounded-tl-sm rounded-bl-sm md:rounded-tr-sm md:rounded-br-sm transition-all duration-500 peer-hover:w-36 peer-hover:visible hover:w-36 hover:visible absolute top-0 right-0 md:right-auto md:left-0 bg-white text-black p-3 text-md">
             <li
               className={`p-0 md:pr-2 invisible w-0 group-hover:w-full group-hover:visible overflow-hidden hover:border-aptitud-petrol ${
                 filter === 'post'
