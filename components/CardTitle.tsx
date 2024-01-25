99100a2d53a8fd5f8d1e8c541148ab0121b159a5
Crafoord <magnus.crafoord@mathem.se>
2024-01-25 12:40:35 +0100

    fix: increase title size on mobile

--- components/CardTitle.tsx
+++ components/CardTitle.tsx
@@ -1,6 +1,6 @@
 export const CardTitle = ({ children }: { children: React.ReactNode }) => {
   return (
-    <h3 className="[text-shadow:_0_5px_0_rgba(0_0_0_/_15%)] mb-2 md:mb-6 text-center -rotate-3 font-shantell italic text-2xl md:text-4xl font-medium truncate">
+    <h3 className="[text-shadow:_0_5px_0_rgba(0_0_0_/_15%)] mb-4 md:mb-6 text-center -rotate-3 font-shantell italic text-3xl md:text-4xl font-medium truncate">
       {children}
     </h3>
   )
