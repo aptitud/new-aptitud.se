e34f905489b257bc5609150986e9cf37215b83c4
Sebastian Näslund <sebastian.karl.naslund@gmail.com>
2024-04-23 21:31:34 +0200

    Try catch

--- lib/domain/instagram/service.ts
+++ lib/domain/instagram/service.ts
@@ -3,10 +3,14 @@ import { REVALIDATE_IN_SECONDS, REVALIDATE_TAGS } from '../../consants'
 const { INSTAGRAM_ACCESS_TOKEN } = process.env
 
 export const getInstagramPosts = async (): Promise<any[]> => {
-  const res = await fetch(
-    `https://graph.instagram.com/me/media?fields=id,caption,media_url,thumbnail_url,permalink,timestamp&access_token=${INSTAGRAM_ACCESS_TOKEN}&limit=16`,
-    { next: { revalidate: REVALIDATE_IN_SECONDS, tags: [REVALIDATE_TAGS.instagram] } }
-  )
-  const posts = await res.json()
-  return posts.data.reverse()
+  try {
+    const res = await fetch(
+      `https://graph.instagram.com/me/media?fields=id,caption,media_url,thumbnail_url,permalink,timestamp&access_token=${INSTAGRAM_ACCESS_TOKEN}&limit=16`,
+      { next: { revalidate: REVALIDATE_IN_SECONDS, tags: [REVALIDATE_TAGS.instagram] } }
+    )
+    const posts = await res.json()
+    return posts.data.reverse()
+  } catch (error) {
+    return []
+  }
 }
