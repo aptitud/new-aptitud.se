959c94f6ac4dfcaeadef49af3c817e92cc726d96
Sebastian Näslund <sebastian.karl.naslund@gmail.com>
2024-05-17 15:08:07 +0200

    Notifty slack

--- app/api/instagram/refresh-token/route.ts
+++ app/api/instagram/refresh-token/route.ts
@@ -12,14 +12,16 @@ export async function GET(request: NextRequest) {
   }
 
   const token = await refreshToken(process.env.INSTAGRAM_ACCESS_TOKEN!)
+  await notifySlack(token)
   return new Response(JSON.stringify(token))
 }
 
-async function refreshToken(token: string): Promise<{
+type InstagramTokenRefreshResponse = {
   access_token: string
   token_type: string
   expires_in: number
-}> {
+}
+async function refreshToken(token: string): Promise<InstagramTokenRefreshResponse> {
   const response = await fetch(
     `https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${token}`
   )
@@ -28,4 +30,28 @@ async function refreshToken(token: string): Promise<{
   }
 
   return response.json()
+}
+
+const notifySlack = async (response: InstagramTokenRefreshResponse) => {
+  const slackWebhook = process.env.SLACK_WEBHOOK
+  if (!slackWebhook) {
+    return
+  }
+
+  // get date from expires_in seconds
+  const expiryDate = new Date(Date.now() + response.expires_in * 1000)
+
+  const expiryDateString = expiryDate.toISOString()
+
+  const message = {
+    text: `Instagram token refreshed. Expires at ${expiryDateString}`,
+  }
+
+  await fetch(slackWebhook, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  })
 }
