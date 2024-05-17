d2484cad2d3b8852e1919eff95d64d28904ddb3a
magnus-crafoord <magnus.crafoord@telenorconnexion.com>
2024-05-17 10:44:15 +0200

    fix: return response

--- app/api/instagram/propagate/route.ts
+++ app/api/instagram/propagate/route.ts
@@ -1,16 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
 import { getInstagramPosts } from '../../../../lib/domain/instagram/service'
 
 export const runtime = 'nodejs'
 
-export async function POST(request: Request, response: Response) {
+export async function POST(request: NextRequest) {
   console.log(request)
   try {
     // Fetch posts from Instagram API
     const instagramPosts = await getInstagramPosts()
 
-    return { message: `Posts propagated successfully, ${instagramPosts.length}`, status: 200 }
+    return new NextResponse(JSON.stringify({ message: `Posts propagated successfully, ${instagramPosts.length}` }), {
+      status: 200,
+    })
   } catch (error) {
     console.error('Error propagating posts:', error)
-    return { message: 'Failed to propagate posts', status: 500 }
+    return new NextResponse(JSON.stringify({ message: `Failed...` }), {
+      status: 500,
+    })
   }
 }
