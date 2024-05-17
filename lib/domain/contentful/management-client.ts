3cad0367891467b732ea8d3f8aaf90d2950b7e36
magnus-crafoord <magnus.crafoord@telenorconnexion.com>
2024-05-17 14:41:06 +0200

    fix: correct import

--- lib/domain/contentful/management-client.ts
+++ lib/domain/contentful/management-client.ts
@@ -1,4 +1,4 @@
-import contentful from 'contentful-management'
+import { createClient } from 'contentful-management'
 
 const { CONTENTFUL_MANAGEMENT_API_TOKEN } = process.env
 if (!CONTENTFUL_MANAGEMENT_API_TOKEN) throw new Error('CONTENTFUL_MANAGEMENT_API_TOKEN is not set')
@@ -6,7 +6,7 @@ if (!CONTENTFUL_MANAGEMENT_API_TOKEN) throw new Error('CONTENTFUL_MANAGEMENT_API
 const { CONTENTFUL_SPACE_ID } = process.env
 if (!CONTENTFUL_SPACE_ID) throw new Error('CONTENTFUL_SPACE_ID is not set')
 
-export const client = contentful.createClient(
+export const client = createClient(
   {
     // This is the access token for this space. Normally you get the token in the Contentful web app
     accessToken: CONTENTFUL_MANAGEMENT_API_TOKEN,
